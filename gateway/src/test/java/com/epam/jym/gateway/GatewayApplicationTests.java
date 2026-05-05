5752e2696ec6a9e14acff0f02e4ec30c1aac9d05
Nijat Gurbanov <nijat_gurbanov@epam.com>
2026-05-05 19:05:30 +0400

    add eureka and gateway modules with configuration and dependencies

---
+++ gateway/src/test/java/com/epam/jym/gateway/GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epam.jym.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
---
+++ user/src/test/java/com/epam/jym/user/UserApplicationTests.java
@@ -0,0 +1,14 @@
+package com.epam.jym.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
+
