3ee6af83ab1d4b22101bf7442428baf3b71c8d02
Nijat Gurbanov <nijat_gurbanov@epam.com>
2026-05-05 11:25:36 +0400

    added authentication ms

---
+++ auth/src/main/java/com/epam/jym/authms/AuthMsApplication.java
@@ -0,0 +1,13 @@
+package com.epam.jym.authms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthMsApplication {
+
+  static void main(String[] args) {
+    SpringApplication.run(AuthMsApplication.class, args);
+  }
+
+}
