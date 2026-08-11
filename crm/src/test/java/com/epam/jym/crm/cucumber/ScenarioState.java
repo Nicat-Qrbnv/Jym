828b5399505bda7c1ecdfa8bf2e6c4dc55e9e145
Nijat Gurbanov <nijat_gurbanov@epam.com>
2026-08-12 01:47:25 +0400

    ## add trainer workload messaging steps and integration tests
    - Implement steps for publishing workload messages in response to training creation and deletion.
    - Create integration test runner for Cucumber tests.
    - Add feature file for testing workload messaging scenarios.
    - Update ScenarioState to track workload messages.

--- crm/src/test/java/com/epam/jym/crm/cucumber/ScenarioState.java
+++ crm/src/test/java/com/epam/jym/crm/cucumber/ScenarioState.java
@@ -1,29 +1,22 @@
 package com.epam.jym.crm.cucumber;
 
+import com.epam.jym.crm.dto.TrainerWorkloadUpdateRequest;
 import io.cucumber.spring.ScenarioScope;
+import java.util.List;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.stereotype.Component;
 import org.springframework.test.web.servlet.MvcResult;
 
+@Setter
+@Getter
 @Component
 @ScenarioScope
 public class ScenarioState {
 
   private MvcResult result;
   private String lastCreatedUsername;
+  private TrainerWorkloadUpdateRequest lastWorkloadMessage;
+  private List<TrainerWorkloadUpdateRequest> lastWorkloadMessages;
 
-  public MvcResult getResult() {
-    return result;
-  }
-
-  public void setResult(MvcResult result) {
-    this.result = result;
-  }
-
-  public String getLastCreatedUsername() {
-    return lastCreatedUsername;
-  }
-
-  public void setLastCreatedUsername(String lastCreatedUsername) {
-    this.lastCreatedUsername = lastCreatedUsername;
-  }
 }
